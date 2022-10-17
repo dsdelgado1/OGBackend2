49a8104e807020c38374b217d71858ab3cf0fff8
Zach Mines <zachmines@Zachs-iMac.local>
2022-10-17 18:19:33 -0400

    added env variables through heroku config

--- util/database.js
+++ util/database.js
@@ -1,13 +1,12 @@
 import mysql from 'mysql2';
-import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
+require('dotenv').config()
 
 
 const pool = mysql.createPool({
     host: 'us-cdbr-east-06.cleardb.net',
     user: 'b1a19a11cc23ca',
     database: 'heroku_bc4830374d63828',
-    password: '35db1c05'
+    password: process.env.DATABASE_PASSWORD
 });
 
 export default pool.promise();
